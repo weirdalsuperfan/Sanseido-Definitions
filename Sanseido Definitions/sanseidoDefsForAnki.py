1dd9b52d82f9d116757b52e7dad540f73bb2c9b2
weirdalsuperfan <weirdalsuperfan@users.noreply.github.com>
2018-11-26 01:38:49 +0900

    fixed nhk compatibility, added production field
    Made it function properly with the new NHK accent plugin
    Now additionally automatically fills out a separate field ProductionDef that gives the definition without revealing the headword, useful for production cards.

--- Sanseido Definitions/sanseidoDefsForAnki.py
+++ Sanseido Definitions/sanseidoDefsForAnki.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 
 """
-	Sanseido Definitions plugin for Anki
-	pulls definitions from sanseido.net's デイリーコンサイス国語辞典
-	
-	Definition fetching adapted from rikaichan.js
-	Field updating modified from Sentence_Gloss.py
-	
-	@author 	= kqueryful
-	@date 		= 1/18/2015
-	@version 	= 1.0
+    Sanseido Definitions plugin for Anki
+    pulls definitions from sanseido.net's デイリーコンサイス国語辞典
+    
+    Definition fetching adapted from rikaichan.js
+    Field updating modified from Sentence_Gloss.py
+    
+    @author     = kqueryful
+    @date       = 1/18/2015
+    @version    = 1.0
 """
 from bs4.BeautifulSoup import BeautifulSoup
 import urllib
@@ -24,6 +24,7 @@ from japanese.reading import MecabController
 # Edit these field names if necessary ==========================================
 expressionField = 'Expression'
 definitionField = 'Sanseido'
+productionField = 'ProductionDef'
 # ==============================================================================
 
 mecab = MecabController()
@@ -33,29 +34,31 @@ def fetchDef(term):
     searched = re.search(r'^[^\[]+',term)
     if searched:
         term = searched.group(0)
-	defText = ""
-	pageUrl = "http://www.sanseido.biz/User/Dic/Index.aspx?TWords=" + urllib.quote(term.encode('utf-8')) + "&st=0&DailyJJ=checkbox"
-	response = urllib.urlopen(pageUrl)
-	soup = BeautifulSoup(response)
-	NetDicBody = soup.find('div', class_ = "NetDicBody")
-	if NetDicBody != None:
-		defFinished = False
-		
-		for line in NetDicBody.children:
-			if line.name == "b":
-				if len(line) != 1:
-					for child in line.children:
-						if child.name == "span":
-							defFinished = True
-			if defFinished:
-				break
-			
-			if line.string != None and line.string != u"\n":
-				defText += line.string
-				
-	defText = re.sub(ur"［(?P<no>[２-９]+)］", ur"<br/><br/>［\1］", defText)
-	if defText: defText = u"　・　<b>定義</b>: " + mecab.reading(defText)
-	return re.sub(ur"（(?P<num>[２-９]+)）", ur"<br/>（\1）", defText)
+    defText = ""
+    pageUrl = "http://www.sanseido.biz/User/Dic/Index.aspx?TWords=" + urllib.quote(term.encode('utf-8')) + "&st=0&DailyJJ=checkbox"
+    response = urllib.urlopen(pageUrl)
+    soup = BeautifulSoup(response)
+    NetDicBody = soup.find('div', class_ = "NetDicBody")
+    if NetDicBody != None:
+        defFinished = False
+        
+        for line in NetDicBody.children:
+            if line.name == "b":
+                if len(line) != 1:
+                    for child in line.children:
+                        if child.name == "span":
+                            defFinished = True
+            if defFinished:
+                break
+            
+            if line.string != None and line.string != u"\n":
+                defText += line.string
+                
+    defText = re.sub(ur"［(?P<no>[２-９]+)］", ur"<br/><br/>［\1］", defText)
+    if defText:
+        defText = u"　・　<b>" + term + "</b>: " + mecab.reading(defText)
+            
+    return re.sub(ur"（(?P<num>[２-９]+)）", ur"<br/>（\1）", defText)
 
 # Update note ==================================================================
 from PyQt4.QtCore import *
@@ -68,36 +71,37 @@ def glossNote( f ):
     if f[ definitionField ]: return
     _, definition = multi_lookup(f[ expressionField ], fetchDef, separator = "")
     f[ definitionField ] = definition.lstrip('　・')
+    f[ productionField ] = re.sub(ur'\<b\>.+?\<\/b\>', ur'<b>定義</b>', definition.lstrip('　・'))
 
 def setupMenu( ed ):
-	a = QAction( 'Regenerate Sanseido definitions', ed )
-	ed.connect( a, SIGNAL('triggered()'), lambda e=ed: onRegenGlosses( e ) )
-	ed.form.menuEdit.addAction( a )
+    a = QAction( 'Regenerate Sanseido definitions', ed )
+    ed.connect( a, SIGNAL('triggered()'), lambda e=ed: onRegenGlosses( e ) )
+    ed.form.menuEdit.addAction( a )
 
 def onRegenGlosses( ed ):
-	n = "Regenerate Sanseido definitions"
-	ed.editor.saveNow()
-	regenGlosses(ed, ed.selectedNotes() )   
-	mw.requireReset()
-	
+    n = "Regenerate Sanseido definitions"
+    ed.editor.saveNow()
+    regenGlosses(ed, ed.selectedNotes() )   
+    mw.requireReset()
+    
 def regenGlosses( ed, fids ):
-	mw.progress.start( max=len( fids ) , immediate=True)
-	for (i,fid) in enumerate( fids ):
-		mw.progress.update( label='Generating Sanseido definitions...', value=i )
-		f = mw.col.getNote(id=fid)
-		try: glossNote( f )
-		except:
-			pass
-			"""import traceback
-			print 'definitions failed:'
-			traceback.print_exc()"""
-		try: f.flush()
-		except:
-			raise Exception()
-			#sleep(10*random.random())
-		ed.onRowChanged(f,f)
-	mw.progress.finish()
-	
+    mw.progress.start( max=len( fids ) , immediate=True)
+    for (i,fid) in enumerate( fids ):
+        mw.progress.update( label='Generating Sanseido definitions...', value=i )
+        f = mw.col.getNote(id=fid)
+        try: glossNote( f )
+        except:
+            pass
+            """import traceback
+            print 'definitions failed:'
+            traceback.print_exc()"""
+        try: f.flush()
+        except:
+            raise Exception()
+            #sleep(10*random.random())
+        ed.onRowChanged(f,f)
+    mw.progress.finish()
+    
 addHook( 'browser.setupMenus', setupMenu )
 
 
